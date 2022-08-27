50660db29c7354158b1c1977d8ecaf99e9c80bfc
Komal Kasyap <Komalkashyap279@gmail.com>
2022-08-27 15:50:53 +0530

    Add existing project to Git

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
+/** 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link
 } from "react-router-dom";
+**/
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import Alert from './components/Alert';
@@ -41,16 +43,17 @@ function App() {
    }
   return (
     <>
-    <Router>
+    {/* <Router> */}
     <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container my-3">
-    <Routes>
+    <Textform heading="Enter the text to be analysed below!!" mode={mode} showAlert={showAlert}/>
+    {/* <Routes>
           <Route path="/about" element={<About/>}/>
           <Route path="/" element={<Textform heading="Enter the text to be analysed below!!" mode={mode} showAlert={showAlert}/>}/>
-    </Routes>
+    </Routes> */}
     </div>
-    </Router>
+    {/* </Router> */}
     </>
   );
 }
